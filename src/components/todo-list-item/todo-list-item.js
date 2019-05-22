225ae549e74f2d6b97df3a6df8030c022dd1a19b
darchievboris <darchievboris@gmail.com>
2019-05-22 15:10:22 -0400

    todo-react-native

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+import './todo-list-item.css'
+export default class TodoListItem extends Component {
+  // state = {
+  //   done: false,
+  //   important: false
+  // }
+  // onLabelClick = () => {
+  //   this.setState(({ done }) => {
+  //     return {
+  //       done: !done
+  //     }
+  //   })
+  // }
+  // onMarkImportant = () => {
+  //   this.setState(state => {
+  //     return {
+  //       important: !state.important
+  //     }
+  //   })
+  // }
+  // constructor () {
+  //   this.onLabelClick = () => {
+  //     console.log(`Done: ${label}`)
+  //   }
+  // }
+  render () {
+    const { label, onDeleted, onToggleImportant, onToggleDone, important, done } = this.props
+    //const { done, important } = this.state
+
+    let className = 'todo-list-item'
+    if (done) {
+      className += ' done'
+    }
+    if (important) {
+      className += ' important'
+    }
+
+    return (
+      <span className={className}>
+        <span className='todo-list-item-label' onClick={onToggleDone}>
+          {label}
+        </span>
+
+        <button
+          type='button'
+          className='btn btn-outline-success btn-sm float-right'
+          onClick={onToggleImportant}
+        >
+          <i className='fa fa-exclamation' />
+        </button>
+
+        <button
+          type='button'
+          className='btn btn-outline-danger btn-sm float-right'
+          onClick={onDeleted}
+        >
+          <i className='fa fa-trash-o' />
+        </button>
+      </span>
+    )
+  }
+}
